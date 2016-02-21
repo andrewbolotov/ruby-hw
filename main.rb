9abe3c6d2e5183a2f2ad4031bf0a427a5fee3f2b
Andrew Bolotov <abolotov@netprospex.com>
2016-02-21 20:31:52 +0200

    homework3

--- main.rb
+++ main.rb
@@ -1,10 +1,8 @@
-inputArray = ARGV
-if inputArray[0] == inputArray[2] and inputArray[1] == inputArray[3]
-	puts "Точка найдена!"
-elsif inputArray[0] == inputArray[2]
-	puts "х координата верна, y нет" 
-elsif inputArray[1] == inputArray[3]
-	puts "y координата верна, x нет" 
-else 
-	puts "Близко, но нет"
-end
+require 'pry'
+require_relative 'classes/developer.rb'
+
+dev = Developer.new('John')
+junDev = JuniorDeveloper.new('Ori')
+senDev = SeniorDeveloper.new('Matt')
+
+binding.pry
